b44976b4e4c569a39fa21fd123de88db823baa21
massini <marco.assini@optimo.it>
2016-03-22 17:21:07 +0100

    first commit

---
+++ app/src/test/java/it/example/assini/myfirstapplication/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package it.example.assini.myfirstapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
