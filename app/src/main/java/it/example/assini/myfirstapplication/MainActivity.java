a15d9c65d93541bb6a3a117c6c9da8e46e9da6e9
massini <marco.assini@optimo.it>
2016-03-26 14:37:20 +0100

    wip

--- app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
+++ app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
@@ -17,6 +18,11 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edit =  (EditText) findViewById(R.id.my_name);
+        //String text = edit.getText.tostring();
+        String text = edit.getText().toString();
+
     }
 
 }
