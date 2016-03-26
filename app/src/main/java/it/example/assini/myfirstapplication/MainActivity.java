f6eb77b2a70f259b9c7d4c5f5cf76b576cdb3542
massini <marco.assini@optimo.it>
2016-03-26 17:58:55 +0100

    wip

--- app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
+++ app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
@@ -2,15 +2,14 @@ package it.example.assini.myfirstapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends Activity
 {
@@ -22,13 +21,59 @@ public class MainActivity extends Activity
     }
 
     public void onButtonTap(View v) {
-        EditText edit =  (EditText) findViewById(R.id.my_name);
-        String text = edit.getText().toString();
+        EditText editName =  (EditText) findViewById(R.id.Name);
+        String name = editName.getText().toString();
 
-        Toast myToast = Toast.makeText(getApplicationContext(), "Ciao " + text +"!", Toast.LENGTH_LONG);
+        EditText editYear =  (EditText) findViewById(R.id.Year);
+        int year = Integer.parseInt(editYear.getText().toString());
+        String anni = getAge(year,1,1);
+
+        if ("Elena".equalsIgnoreCase(name.trim())){
+            name = "Amorino";
+            anni = "giovane";
+        }
+
+        Toast myToast = Toast.makeText(getApplicationContext(), "Ciao " + name +"! (anni: " +anni+")" , Toast.LENGTH_LONG);
+        myToast.show();
+
+    }
+
+    public void onDateTap(View v) {
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c.getTime());
+
+        Toast myToast = Toast.makeText(getApplicationContext(), "Oggi è il " + formattedDate, Toast.LENGTH_LONG);
         myToast.show();
 
 
+    }
+
+
+
+
+    /**
+     * Method to extract the user's age from the entered Date of Birth.
+     * @param DoB String The user's date of birth.
+     * @return ageS String The user's age in years based on the supplied DoB.
+     */
+    private String getAge(int year, int month, int day){
+        Calendar dob = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+
+        dob.set(year, month, day);
+
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)){
+            age--;
+        }
+
+        Integer ageInt = new Integer(age);
+        String ageS = ageInt.toString();
+
+        return ageS;
     }
 
 }
