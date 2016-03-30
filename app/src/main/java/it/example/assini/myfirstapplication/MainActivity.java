ad2940291ea10f4e46947f6b6437193cb98ae7e0
massini <marco.assini@optimo.it>
2016-03-30 18:12:29 +0200

    wip

--- app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
+++ app/src/main/java/it/example/assini/myfirstapplication/MainActivity.java
@@ -1,34 +1,84 @@
 package it.example.assini.myfirstapplication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
+    DatePicker pickerDate;
+    TextView info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+
+//        if(!"".equals(name) && !"".equals(year)){
+//            btn.setClickable(true);
+//        }
+
+
+
+        info = (TextView)findViewById(R.id.info);
+        pickerDate = (DatePicker)findViewById(R.id.pickerdate);
+
+        Calendar today = Calendar.getInstance();
+
+        pickerDate.init(
+                today.get(Calendar.YEAR),
+                today.get(Calendar.MONTH),
+                today.get(Calendar.DAY_OF_MONTH),
+                new OnDateChangedListener(){
+
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear,int dayOfMonth) {
+
+                        Toast.makeText(getApplicationContext(), "onDateChanged", Toast.LENGTH_SHORT).show();
+
+                        info.setText(
+                                "Year: " + year + "\n" +
+                                "Month of Year: " + monthOfYear + "\n" +
+                                "Day of Month: " + dayOfMonth);
+
+                    }
+                }
+        );
+
     }
 
     public void onButtonTap(View v) {
+
         EditText editName =  (EditText) findViewById(R.id.Name);
         String name = editName.getText().toString();
 
+        String anni;
+        int year = 0;
         EditText editYear =  (EditText) findViewById(R.id.Year);
-        int year = Integer.parseInt(editYear.getText().toString());
-        String anni = getAge(year,1,1);
+        if (!TextUtils.isEmpty(editYear.getText().toString())){
+            year = Integer.parseInt(editYear.getText().toString());
+            anni = getAge(year, 1, 1);
+        } else {
+            anni = "?";
+        }
 
-        if ("Elena".equalsIgnoreCase(name.trim())){
+        if ("Elena".equalsIgnoreCase(name.trim()) && 1981==year){
             name = "Amorino";
             anni = "giovane";
         }
@@ -41,8 +91,11 @@ public class MainActivity extends Activity
     public void onDateTap(View v) {
 
         Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        SimpleDateFormat df;
+        df = new SimpleDateFormat("dd-MMM-yyyy");
         String formattedDate = df.format(c.getTime());
+
+        sendEmail();
 
         Toast myToast = Toast.makeText(getApplicationContext(), "Oggi è il " + formattedDate, Toast.LENGTH_LONG);
         myToast.show();
@@ -50,13 +103,8 @@ public class MainActivity extends Activity
 
     }
 
-
-
-
     /**
      * Method to extract the user's age from the entered Date of Birth.
-     * @param DoB String The user's date of birth.
-     * @return ageS String The user's age in years based on the supplied DoB.
      */
     private String getAge(int year, int month, int day){
         Calendar dob = Calendar.getInstance();
@@ -75,5 +123,31 @@ public class MainActivity extends Activity
 
         return ageS;
     }
+
+    protected void sendEmail() {
+        Log.i("Send email", "Send email");
+
+        String[] TO = {"marco.assini@gmail.com"};
+        //String[] CC = {"xyz@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "MyFirstApplication");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "bla bla bla");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Send email", "Finished sending email...");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 }
